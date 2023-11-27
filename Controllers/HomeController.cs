8a7a7071f6fe091828d714bd34e9d2c66fcb55b8
RTDMakler <rtdmakler@gmail.com>
2023-11-27 17:07:25 +0300

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics;
+using Cafe.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cafe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // Создаем список для передачи ссылок в представление
+            var links = new List<string>
+            {
+                Url.Action("Page1"),
+                Url.Action("Page2"),
+                Url.Action("Page3"),
+                Url.Action("Page4")
+            };
+
+            // Создаем список для передачи изображений в представление
+            var images = new List<string>();
+            for (int i = 1; i <= 10; i++)
+            {
+                images.Add($"~/images/image{i}.png");
+            }
+
+            // Создаем модель, содержащую данные
+            var model = new HomeViewModel
+            {
+                Links = links,
+                Images = images
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Cafe.Models
+{
+    public class HomeViewModel
+    {
+        public List<string> Links { get; set; }
+        public List<string> Images { get; set; }
+    }
+}
